891f237cb3f11f876aa6b12228924e7d22202ee3
dicecd00 <107855582+dicecd00@users.noreply.github.com>
2024-12-01 15:52:50 -0900

    Add files via upload

--- Skyrim Potion Calculator.cpp
+++ Skyrim Potion Calculator.cpp
@@ -15,17 +15,185 @@ class ingredient {
   int effect_2_mag;
   int effect_2_dur;
   int effect_2_cost;
+  double effect_2_valuemod;
   
   string effect_3;
   int effect_3_mag;
   int effect_3_dur;
   int effect_3_cost;
+  double effect_3_valuemod;
   
   string effect_4;
   int effect_4_mag;
   int effect_4_dur;
   int effect_4_cost;
+  double effect_4_valuemod;
 };
+
+class potion {
+  public:
+  string pEffect_1;
+  int pEffect_1_mag;
+  int pEffect_1_dur;
+  
+  string pEffect_2;
+  int pEffect_2_mag;
+  int pEffect_2_dur;
+
+  string pEffect_3;
+  int pEffect_3_mag;
+  int pEffect_3_dur;
+
+  string pEffect_4;
+  int pEffect_4_mag;
+  int pEffect_4_dur;
+
+  double pValue;
+};
+
+
+
+
+//object declarations for use in combinePotions and main
+ingredient nullIngredient;
+potion potionToCombine;
+
+/*function that takes 2 ingredients as parameters and an optional third to determine if the ingredients can be combined to form a potion and
+if so, outputs the resulting potion and its associated potion data */
+potion combinePotions(ingredient i1, ingredient i2, ingredient i3 = nullIngredient)
+{
+    
+    
+    
+    //compare ingredient effects to determine any common effects
+    if (i1.effect_1 == i2.effect_1 || i1.effect_1 == i2.effect_2 || i1.effect_1 == i2.effect_3 || i1.effect_1 == i2.effect_4
+     || i1.effect_1 == i3.effect_1 || i1.effect_1 == i3.effect_2 || i1.effect_1 == i3.effect_3 || i1.effect_1 == i3.effect_4) {
+        potionToCombine.pEffect_1 = i1.effect_1;
+    }    
+    
+    //combination logic pt2
+    if (i1.effect_2 == i2.effect_1 || i1.effect_2 == i2.effect_2 || i1.effect_2 == i2.effect_3 || i1.effect_2 == i2.effect_4
+     || i1.effect_2 == i3.effect_1 || i1.effect_2 == i3.effect_2 || i1.effect_2 == i3.effect_3 || i1.effect_2 == i3.effect_4) {
+        
+        //if the ingredients already have one effect in common, assigns the second potion effect to the ingredient in common
+        if (potionToCombine.pEffect_1 != i1.effect_1) {
+            potionToCombine.pEffect_1 = i1.effect_2;
+        }
+        else {
+        potionToCombine.pEffect_2 = i1.effect_2;
+        }
+    }
+    
+    //combination logic pt3
+    if (i1.effect_3 == i2.effect_1 || i1.effect_3 == i2.effect_2 || i1.effect_3 == i2.effect_3 || i1.effect_3 == i2.effect_4
+     || i1.effect_3 == i3.effect_1 || i1.effect_3 == i3.effect_2 || i1.effect_3 == i3.effect_3 || i1.effect_3 == i3.effect_4) {
+        
+        //if the ingredients already have one/two effect(s) in common, assigns the second/third potion effect to the ingredient in common
+        //could replace OR statements with comparisons to nullIngredient
+        if (potionToCombine.pEffect_1 == nullIngredient.effect_1) {
+            potionToCombine.pEffect_1 = i1.effect_3;
+        }
+        else if (potionToCombine.pEffect_2 == nullIngredient.effect_1) {
+        potionToCombine.pEffect_2 = i1.effect_3;
+        }
+        else {
+            potionToCombine.pEffect_3 = i1.effect_3;
+        }
+    }
+    
+    //combination logic pt4
+    if (i1.effect_4 == i2.effect_1 || i1.effect_4 == i2.effect_2 || i1.effect_4 == i2.effect_3 || i1.effect_4 == i2.effect_4
+     || i1.effect_4 == i3.effect_1 || i1.effect_4 == i3.effect_2 || i1.effect_4 == i3.effect_3 || i1.effect_4 == i3.effect_4) {
+        
+        //if the ingredients already have one/two/three effect(s) in common, assigns the second/third/fourth potion effect to the ingredient in common
+        if (potionToCombine.pEffect_1 == nullIngredient.effect_1) {
+            potionToCombine.pEffect_1 = i1.effect_4;
+        }
+        else if (potionToCombine.pEffect_2 == nullIngredient.effect_1) {
+        potionToCombine.pEffect_2 = i1.effect_4;
+        }
+        else if (potionToCombine.pEffect_3 == nullIngredient.effect_1){
+            potionToCombine.pEffect_3 = i1.effect_4;
+        }
+        else {
+            potionToCombine.pEffect_4 = i1.effect_4;
+        }
+    }
+    
+    //combination logic for exclusively ingredient 2 and 3
+    if (i2.effect_1 == i3.effect_1 || i2.effect_1 == i3.effect_2 || i2.effect_1 == i3.effect_3 || i2.effect_1 == i3.effect_4) { 
+    
+        //if the ingredients already have one/two/three effect(s) in common, assigns the second/third/fourth potion effect to the ingredient in common
+        if (potionToCombine.pEffect_1 == nullIngredient.effect_1) {
+            potionToCombine.pEffect_1 = i2.effect_1;
+        }
+        else if (potionToCombine.pEffect_2 == nullIngredient.effect_1) {
+            potionToCombine.pEffect_2 = i2.effect_1;
+        }
+        else if (potionToCombine.pEffect_3 == nullIngredient.effect_1){
+            potionToCombine.pEffect_3 = i2.effect_1;
+        }
+        else {
+            potionToCombine.pEffect_4 = i2.effect_1;
+        }
+    }
+    
+    if (i2.effect_2 == i3.effect_1 || i2.effect_2 == i3.effect_2 || i2.effect_2 == i3.effect_3 || i2.effect_2 == i3.effect_4) { 
+        
+        //if the ingredients already have one/two/three effect(s) in common, assigns the second/third/fourth potion effect to the ingredient in common
+        if (potionToCombine.pEffect_1 == nullIngredient.effect_1) {
+            potionToCombine.pEffect_1 = i2.effect_2;
+        }
+        else if (potionToCombine.pEffect_2 == nullIngredient.effect_1) {
+            potionToCombine.pEffect_2 = i2.effect_2;
+        }
+        else if (potionToCombine.pEffect_3 == nullIngredient.effect_1){
+            potionToCombine.pEffect_3 = i2.effect_2;
+        }
+        else {
+            potionToCombine.pEffect_4 = i2.effect_2;
+        }
+    }
+    
+    if (i2.effect_3 == i3.effect_1 || i2.effect_3 == i3.effect_2 || i2.effect_3 == i3.effect_3 || i2.effect_3 == i3.effect_4) { 
+        
+        //if the ingredients already have one/two/three effect(s) in common, assigns the second/third/fourth potion effect to the ingredient in common
+        if (potionToCombine.pEffect_1 == nullIngredient.effect_1) {
+            potionToCombine.pEffect_1 = i2.effect_3;
+        }
+        else if (potionToCombine.pEffect_2 == nullIngredient.effect_1) {
+            potionToCombine.pEffect_2 = i2.effect_3;
+        }
+        else if (potionToCombine.pEffect_3 == nullIngredient.effect_1){
+            potionToCombine.pEffect_3 = i2.effect_3;
+        }
+        else {
+            potionToCombine.pEffect_4 = i2.effect_3;
+        }
+    }
+    
+    if (i2.effect_4 == i3.effect_1 || i2.effect_4 == i3.effect_2 || i2.effect_4 == i3.effect_3 || i2.effect_4 == i3.effect_4) { 
+    
+        //if the ingredients already have one/two/three effect(s) in common, assigns the second/third/fourth potion effect to the ingredient in common
+        if (potionToCombine.pEffect_1 == nullIngredient.effect_1) {
+            potionToCombine.pEffect_1 = i2.effect_4;
+        }
+        else if (potionToCombine.pEffect_2 == nullIngredient.effect_1) {
+            potionToCombine.pEffect_2 = i2.effect_4;
+        }
+        else if (potionToCombine.pEffect_3 == nullIngredient.effect_1){
+            potionToCombine.pEffect_3 = i2.effect_4;
+        }
+        else {
+            potionToCombine.pEffect_4 = i2.effect_4;
+        }
+    }
+    
+    return potionToCombine;
+}
+
+
+
 
 int main() 
 {
@@ -55,7 +223,8 @@ int main()
     cout << " | " << ingred_1.effect_4_cost;
     cout << endl << endl;
     
-    /* Restore Health
+/* 
+Restore Health
 10
 1
 1
@@ -86,8 +255,11 @@ Slow
 Paralyze
 1
 10
-1*/
+1
+*/
     
+    
+    //manual ingredient entry to be replaced by file reading function
     cout << "Enter 4 effects by effect name, magnitude, duration and value modifier." << endl << endl;
     cout << "Ingredient 2" << endl << "=========================================================" << endl;
     cin.ignore();
@@ -109,12 +281,12 @@ Paralyze
     cout << setw(17) << ingred_2.effect_3 << " | " << setw(10) << ingred_2.effect_3_mag << "| " << setw(8) << ingred_2.effect_3_dur;
     cout << " | " << ingred_2.effect_3_cost << endl;
     cout << setw(17) << ingred_2.effect_4 << " | " << setw(10) << ingred_2.effect_4_mag << "| " << setw(8) << ingred_2.effect_4_dur;
-    cout << " | " << ingred_2.effect_4_cost;
+    cout << " | " << ingred_2.effect_4_cost << endl;
     
-    
-// code block for potion combination logic
-    
-    
+    combinePotions(ingred_1, ingred_2);
+    cout << endl << "Resulting potion:" << endl;
+    cout << potionToCombine.pEffect_1 << " + " << potionToCombine.pEffect_2 << " + " << potionToCombine.pEffect_3 << " + " << potionToCombine.pEffect_4 << endl;
+
     
     return 0;
 }
